d4ab0130561accf533b930856e7162f749bf1e0d
Manuel David <mdalfaro@bch.bancodechile.cl>
2024-02-22 11:46:43 -0300

    Ajusta datos de db local

--- Recruitment Platform/src/config/database.js
+++ Recruitment Platform/src/config/database.js
@@ -3,7 +3,7 @@ const mysql2 = require('mysql2');
 const pool = mysql2.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: 'admin123',
+    password: 'rootroot',
     database: 'recruitment_db',
     connectionLimit: 100,
 });

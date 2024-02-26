a42ee91fa9f58a846e11ae7dd7bc152e8f93f86f
jacobvegatoro <35665615+jacobvegatoro@users.noreply.github.com>
2024-02-26 14:39:30 -0300

    Jvega (#5)
    * Eliminado directorio node_modules/
    
    * Se mueven archivos de proyecto a directorio raiz
    
    * Eliminar directorio Recruitment Platform
    
    * Actualización app.js

---
+++ src/app.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const postulanteRoutes = require('./routes/postulanteRoutes');
+const clienteRoutes = require('./routes/clienteRoutes');
+const celulaRoutes = require('./routes/celulaRoutes');
+const rolRoutes = require('./routes/rolRoutes');
+const procesoRoutes = require('./routes/procesoRoutes');
+const entrevistaRoutes = require('./routes/entrevistaRoutes');
+const preguntaRoutes = require('./routes/preguntaRoutes');
+const respuestaRoutes = require('./routes/respuestaRoutes');
+const rolUsuarioRoutes = require('./routes/rolUsuarioRoutes');
+const usuarioRoutes = require('./routes/usuarioRoutes');
+const estadoRoutes = require('./routes/estadoRoutes');
+const estadoProcesoRoutes = require('./routes/estadoProcesoRoutes');
+const loginRoutes = require('./routes/authRoutes');
+const estadisticaRoute = require('./routes/estadisticaRoutes');
+
+app.use('/postulantes', postulanteRoutes);
+app.use('/clientes', clienteRoutes);
+app.use('/celulas', celulaRoutes);
+app.use('/roles', rolRoutes);
+app.use('/procesos', procesoRoutes);
+app.use('/entrevistas', entrevistaRoutes);
+app.use('/preguntas', preguntaRoutes);
+app.use('/respuestas', respuestaRoutes);
+app.use('/rolesusuarios', rolUsuarioRoutes);
+app.use('/usuarios', usuarioRoutes);
+app.use('/estados', estadoRoutes);
+app.use('/estadoprocesos', estadoProcesoRoutes);
+app.use('/login', loginRoutes);
+app.use('/estadisticas', estadisticaRoute )
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Ruta no encontrada' });
+});
+
+app.use((err, req, res, next) => {
+    console.error('Error inesperado:', err);
+    res.status(500).json({ message: 'Error interno del servidor' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en el puerto ${process.env.PORT}`);
+    console.log(`Secreto: ${process.env.JWT_SECRET_KEY}`)
+});
